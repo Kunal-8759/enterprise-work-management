768bb8f035763b90b6382339ecaabf06648230f1
Kunal Kumar <kumarkunal8759@gmail.com>
2026-04-23 08:45:47 +0530

    configured standardized way to use CSS

--- frontend/src/HealthCheck.jsx
+++ frontend/src/HealthCheck.jsx
@@ -1,24 +1,19 @@
-
-function HealthCheck(){
-
+function HealthCheck() {
     return (
-        <>
-            <div className="min-h-screen flex flex-col items-center justify-center bg-gray-100 dark:bg-gray-900">
-                <div className="bg-white dark:bg-gray-800 rounded-2xl shadow-lg p-10 flex flex-col items-center gap-4">
-                <h1 className="text-3xl font-bold text-blue-600 dark:text-blue-400">
+        <div className="page-wrapper">
+            <div className="card">
+                <h1 className="heading-primary">
                     Enterprise Work Management System
                 </h1>
-                <p className="text-gray-500 dark:text-gray-300 text-sm">
+                <p className="text-secondary">
                     Tailwind v4 is working
                 </p>
-                <span className="px-4 py-1 rounded-full bg-green-100 text-green-700 text-xs font-semibold">
+                <span className="badge-success">
                     App Running
                 </span>
-                </div>
             </div>
-        </>
-
-    )
+        </div>
+    );
 }
 
 export default HealthCheck;
